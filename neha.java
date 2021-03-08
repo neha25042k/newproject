a298879b3d073371155ae98068d1e01ff7630b5a
neha_rajesh <neha.bmsit.cse@gmail.com>
2021-03-08 20:03:04 +0530

    second commit

--- neha.java
+++ neha.java
@@ -1,5 +1,6 @@
 class neha{
 	public static void main(String args[]){
 		System.out.println("this is my understood vi scripts");
+		System.out.println("this is the second deployement @12pm");
 	}
 }
